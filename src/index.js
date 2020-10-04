377faed787d8cf28b05674cba04fa438e2ed4e8f
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-03 17:04:58 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 console.log('%c HI', 'color: firebrick')
 
+let breeds = []
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchImages();
   fetchBreeds();
 });
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
 function fetchImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
   .then(response => response.json())
   .then(json => displayImages(json))
 };
 
 function fetchBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   fetch(breedUrl)
   .then(response => response.json())
-  .then(json => displayBreeds(json))
+  .then(json => {
+    breeds = Object.keys(json.message);
+    updateBreedList(breeds);
+    addBreedSelectListener();
+  });
 };
 
 function displayImages(images) {
@@ -29,33 +34,35 @@ function displayImages(images) {
   })
 };
 
-function displayBreeds(breeds) {
+function addBreed(breed) {
   const breedList = document.getElementById('dog-breeds');
-  for (const [key, value] of Object.entries(breeds.message)) {
-    const newItem = document.createElement('li');
-    newItem.innerText = key;
-    breedList.appendChild(newItem);
-    newItem.addEventListener('click', function() {
-      newItem.style.color = 'green';
-    })
+  const newItem = document.createElement('li');
+  newItem.innerText = breed;
+  breedList.appendChild(newItem);
+  newItem.addEventListener('click', function() {
+    newItem.style.color = 'green';
+  })
+};
+
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 };
 
-let dropDownBreed = document.getElementById('breed-dropdown');
-dropDownBreed.onChange = function() {
-  let x = document.getElementById("dropDownBreed").value;
-  if (x === "a") {
-    let tagz = document.getElementsByTagName('li')
-    tagz.forEach(tag => {
-      if (tag.text.startsWith != "a") {
-        tag.remove();
-      }
-    })
-  }
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
 }
 
-function testing() {
-  if (dropDownBreed.selectedIndex = "1") {
-    newItem.remove();
-  }
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
 }
