4d8722442600a98fd4f8a5cda746265187b19e57
JacksonReynolds <jwreynod@gmail.com>
2020-10-07 14:56:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+
+function getContent(url, filter = null) {
+    return fetch(url)
+    .then(function(response) {return response.json()})
+    .then(function(json) {
+        if (url.includes('random')) {
+            addImagesToDOM(json)
+        } else if (url.includes('breeds/list')) {
+            addBreedsToDOM(json, filter)
+        }
+    })
+}
+
+function addImagesToDOM(imagesJSON) {
+    imagesJSON.message.forEach(element => {
+        const newImg = dogImgContainer.appendChild(document.createElement('img'))
+        newImg.src = element
+        dogImgContainer.innerHTML += '<br>'
+    })
+    
+}
+
+function addBreedsToDOM(breedsJSON, filter) {
+    console.log(filter)
+    Object.entries(breedsJSON.message).forEach(breed => {
+        if (filter != null && breed.toString().startsWith(filter)) {
+            const newBreed = dogBreedUl.appendChild(document.createElement('li'))
+            newBreed.innerText = breed
+        }
+    })
+}
+
+const contentUrls = ["https://dog.ceo/api/breeds/image/random/4", 'https://dog.ceo/api/breeds/list/all']
+
+
+const dogImgContainer = document.querySelector('#dog-image-container')
+const dogBreedUl = document.querySelector('#dog-breeds')
+const breedDropdown = document.querySelector('#breed-dropdown')
+
+getContent(contentUrls[0])
+
+breedDropdown.addEventListener('change', function(e) {
+    dogBreedUl.innerHTML = ''
+    getContent(contentUrls[1], breedDropdown.value)
+})
+
