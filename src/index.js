a829796ca50865c0b70de349732a3d6eb1f3d4e4
Ethan <eleetyson@gmail.com>
2020-10-12 09:06:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
-console.log('%c HI', 'color: firebrick')
+// fetch the pictures
+function fetchImages() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => addImages(json))
+}
+
+// add each picture to the page as an image
+function addImages(images) {
+  let imgContainer = document.querySelector("div#dog-image-container")
+
+  images.message.forEach(image => {
+    let img = document.createElement("img")
+    img.src = image
+    img.height = 100
+    img.width = 100
+    imgContainer.appendChild(img)
+  })
+}
+
+// fetch the breeds
+function fetchBreeds() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+  .then(resp => resp.json())
+  .then(json => addBreeds(json))
+}
+
+// add each breed to the page as a list item
+// also invokes changeColor() to add an event listener to each li
+// and toggle()...
+function addBreeds(breeds) {
+  let ul = document.querySelector("ul#dog-breeds")
+
+  Object.keys(breeds.message).forEach(breed => {
+    let li = document.createElement("li")
+    li.innerHTML = breed
+    ul.appendChild(li)
+    changeColor()
+    filter()
+  })
+}
+
+// change list item's color on click
+function changeColor() {
+  let li = document.querySelectorAll("ul#dog-breeds li")
+
+  li.forEach(item => {
+    item.addEventListener("click", function(e) {
+      e.target.style.color = "blue"
+    })
+  })
+}
+
+// after loading all dog breeds on to the page
+// allow the user to filter the breed list based on dropdown letter selection
+// *can't iterate over HTMLCollection or NodeList with forEach*
+function filter() {
+  let select = document.getElementById("breed-dropdown")
+
+  select.addEventListener("change", function(e) {
+    let options = document.getElementsByTagName("li")
+
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].innerHTML.startsWith(e.target.value)) {
+        options[i].style.display = ""
+      } else {
+        options[i].style.display = "none"
+      }
+    }
+
+  })
+}
+
+// when the DOM loads, invoke functions to fetch the pictures and breeds
+document.addEventListener("DOMContentLoaded", function() {
+  fetchImages()
+  fetchBreeds()
+})
