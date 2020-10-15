0261f1f2385c7b9385652d6354b5594aad97a3d7
Amanda-Katherine <walker.akw@gmail.com>
2020-10-14 22:30:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(json => renderImages(json))
+}
+
+function renderImages(images) {
+    const main = document.querySelector('main')
+    images.forEach(image => {
+        const img = document.createElement('img')
+        img.innerHTML = image.img
+        main.appendChild(img)
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+})
